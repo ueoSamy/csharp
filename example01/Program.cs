a90eff9a459df4bdbc40aff65ec0799a71d3f863
Samy <c5.samy@mail.ru>
2023-03-20 05:43:17 +0300

    ДЗ за урок 1

--- example01/Program.cs
+++ example01/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Cдавать ссылкой на github
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("ЗАДАЧА 2");
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} больше, {num2} меньше");
+}
+else
+{
+    Console.WriteLine($"{num2} больше, {num1} меньше");
+}
+
+Console.WriteLine("ЗАДАЧА 4");
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("второе первое число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("ЗАДАЧА 6");
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
+
+
+Console.WriteLine("ЗАДАЧА 8");
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Введите число N: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < num1; i++) {
+    if (i % 2 == 0) {
+        Console.WriteLine($"{i} - четное");
+
+    }
+    else {
+        Console.WriteLine($"{i} - нечетное");
+    }
+}
